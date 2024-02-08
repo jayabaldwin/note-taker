fbe9db14f6e6d934ae847d828de76a492cc52fe7
Jaya <148409249+jayabaldwin@users.noreply.github.com>
2024-02-08 17:48:21 +1100

    fixed up code and added further comments

--- routes/notes.js
+++ routes/notes.js
@@ -1,19 +1,22 @@
 const router = require('express').Router();
 const { readFromFile, readAndAppend, readAndDelete } = require('../helpers/fsUtils');
+// Import random id generator
 const uuid = require('../helpers/uuid');
 
-// GET Route for retrieving all the tips
+// GET Route for retrieving user input into the notes, stored in the db.json file
 router.get('/', (req, res) => {
+  console.info(`${req.method} request received for notes`);
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-// POST Route for a new UX/UI tip
+// POST Route for a new note
 router.post('/', (req, res) => {
   console.log(req.body);
 
+  // Destructuring assignment for the items in req.body
   const { title, text } = req.body;
 
-  if ( title && text) {
+  if ( title && text ) {
     const newNote = {
       title,
       text,
@@ -27,6 +30,7 @@ router.post('/', (req, res) => {
   }
 });
 
+// Route to delete note based on id
 router.delete('/:id', (req, res) => {
   readAndDelete(req.params.id, "./db/db.json");
   res.json("Note deleted!");
