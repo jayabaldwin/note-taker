5484326556f448463e8c1720823c4da96e6dcd6e
Jaya <148409249+jayabaldwin@users.noreply.github.com>
2024-02-08 22:19:22 +1100

    added * back

--- server.js
+++ server.js
@@ -25,14 +25,15 @@ app.use(express.static('public'));
 
 
 // HTML routes
-// GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
 
 // GET Route for notes page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// GET Route for homepage
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 
